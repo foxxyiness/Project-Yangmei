1b80a7f482868fa6c227336d5cb95f1e9fc49918
foxxyiness <coachfoxjp@gmail.com>
2021-04-19 18:23:41 -0400

    Created Ammo and Reloading
    made ammo and reloading in the gun script

--- Project Yangmei/Project Yangmei/Project Yangmei 1/Assets/Scripts/Gun.cs
+++ Project Yangmei/Project Yangmei/Project Yangmei 1/Assets/Scripts/Gun.cs
@@ -7,6 +7,15 @@ public class Gun : MonoBehaviour
     [Header("Gun Fire and More")]
     public float damage = 25f;
     public float range = 100f;
+    public float fireRate = 15;
+    public float impactForce = 30f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading;
+
+    private float nextTimeToFire = 0f;
 
     [Header("Camera")]
     public Camera fpsCam;
@@ -15,18 +24,43 @@ public class Gun : MonoBehaviour
     public ParticleSystem muzzleFlash;
     public Transform muzzle;
 
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (isReloading)
+            return;
+
+        if(currentAmmo <= 0)
         {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
     void Shoot()
     {
+
+        currentAmmo --;
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
