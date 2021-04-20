758480475e0b9005bdeb39eb40a95c93509407ee
foxxyiness <coachfoxjp@gmail.com>
2021-04-20 04:03:08 -0400

    Pause Menu

---
+++ Project Yangmei/Project Yangmei/Project Yangmei 1/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject ui;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Toggle();
+    }
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+        if (ui.activeSelf)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+
+    public void Retry()
+    {
+        Toggle();
+        SceneManager.LoadScene(0);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
