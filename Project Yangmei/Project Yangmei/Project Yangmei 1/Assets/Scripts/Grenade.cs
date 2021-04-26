f3802a70f652ec1b8a4caa228601f80619599264
foxxyiness <coachfoxjp@gmail.com>
2021-04-26 19:45:24 -0400

    Created Grenade :D

---
+++ Project Yangmei/Project Yangmei/Project Yangmei 1/Assets/Scripts/Grenade.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3f;
+    public float radius = 5f;
+    public float force = 700f;
+    public float damage = 90f;
+    bool hasExploded;
+    float countdown;
+
+    public GameObject explosionEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+
+        foreach (Collider nearbyObject in collidersToDestroy)
+        {
+            Target enemy = nearbyObject.GetComponent<Target>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
